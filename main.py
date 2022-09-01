ddba364c3c2ce20b392cb0f74bb15e001e532ea5
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-09-01 09:45:38 -0500

    fixed bug preventing whitespace from being converted

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
 
-morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..']
+morse = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-', '.-.', '...', '-', '..-', '...-', '.--', '-..-', '-.--', '--..', ' ']
 
 
 def convert(letter): 
     for i in letter:
-        letters = converter_dict[i]
-        converted_morse.append(letters)
+        if i == ' ':
+            i == ' '
+        else:
+            letters = converter_dict[i]
+            converted_morse.append(letters)
 
-converter_dict = {alphabet[i]: morse[i] for i in range(len(morse))}
+converter_dict = {alphabet[i]: morse[i] for i in range(0,26)}
 converted_morse = []
 
 word = input("Please enter a word: ")
-letter = [i for i in word]
+lowercase_word = word.lower()
+letter = [i for i in lowercase_word]
 
 convert(letter)
 morse_output = ' '.join([str(item) for item in converted_morse])
